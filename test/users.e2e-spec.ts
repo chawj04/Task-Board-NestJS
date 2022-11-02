6585fd2eee7bda668620fc51b9c62a92f066b1fc
chawj04 <chawj0427@gmail.com>
2022-11-02 21:39:35 +0900

    Users_E2E_TEST

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -7,27 +7,24 @@ import { DataSource } from 'typeorm';
 describe('Users-Controller E2E TEST', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleRef.createNestApplication();
     // Test 수행 전, DB synchronize로 DB 초기화
-    await app.get(DataSource).synchronize(true);
+    // await app.get(DataSource).synchronize(true);
     await app.init();
   });
 
-  // Authentication
-  // it('should login', () => {
-  //   return request(app.getHttpServer()).post('/users/signin').send({
-  //     email: 'testingEmail@google.com',
-  //     password: 'testingPassword123',
-  //   });
-  // });
+  // Test 수행 후, DB synchronize로 DB 초기화
+  afterAll(async () => {
+    await app.get(DataSource).synchronize(true);
+  });
 
   // SignUp_User
-  it('Post: SignUp_Success', async () => {
+  it('POST: SignUp_Success', async () => {
     const result = await request(app.getHttpServer())
       .post('/users/signup')
       .send({
@@ -36,11 +33,10 @@ describe('Users-Controller E2E TEST', () => {
         password: 'testingPassword123',
         role: 'admin',
       });
-    // console.log(result);
     expect(result.statusCode).toBe(201);
     expect(result.body.success).toBe(true);
   });
-  it('Post: SignUp_Failed', async () => {
+  it('POST: SignUp_Bad_Request_Failed', async () => {
     const result = await request(app.getHttpServer())
       .post('/users/signup')
       .send({
@@ -55,25 +51,22 @@ describe('Users-Controller E2E TEST', () => {
   });
 
   // SginIn_User
-  it('Post: SignIn_Success', async () => {
+  let accessToken: string; // 상위 Scope -> accessToken 변수 저장
+  it('POST: SignIn_Success', async () => {
     const result = await request(app.getHttpServer())
       .post('/users/signin')
       .send({
         email: 'testingEmail@google.com',
         password: 'testingPassword123',
       });
-    // console.log(result);
-    // console.log(result.body.data.access_token);
-    // const accessToken = result.body.data.access_token;
-    // console.log(accessToken);
+    accessToken = result.body.data.access_token;
     expect(result.statusCode).toBe(201);
     expect(result.body.success).toBe(true);
   });
-  it('Post: SignIn_Failed', async () => {
+  it('POST: SignIn_Bad_Request_Failed', async () => {
     const result = await request(app.getHttpServer())
       .post('/users/signin')
       .send({ email: 'vxsets11@google.com' });
-    // console.log(result);
     expect(result.statusCode).toBe(400);
     expect(result.body.message).toStrictEqual([
       'Please enter a valid password',
@@ -82,38 +75,96 @@ describe('Users-Controller E2E TEST', () => {
   });
 
   // Find_User
+  it('GET: Find_User_Success', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/users/1')
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(result.statusCode).toBe(200);
+    expect(result.body.success).toBe(true);
+  });
+  it('GET: Find_User_Unauthorized_Failed', async () => {
+    const result = await request(app.getHttpServer()).get('/users/1');
+    expect(result.statusCode).toBe(401);
+    expect(result.body.message).toStrictEqual('Unauthorized');
+  });
 
   // Find_All_User
   it('GET: Find_All_User_Success', async () => {
-    const userInfo = await request(app.getHttpServer())
-      .post('/users/signin')
-      .send({
-        email: 'testingEmail@google.com',
-        password: 'testingPassword123',
-      });
-    // console.log(token.body.data.access_token);
-    const accessToken = userInfo.body.data.access_token;
-    // console.log(accessToken);
-
     const result = await request(app.getHttpServer())
       .get('/users?page=1&pageSize=5')
       .set('Authorization', `Bearer ${accessToken}`);
     // console.log(result);
     expect(result.statusCode).toBe(200);
+    expect(result.body.success).toBe(true);
   });
-  it('GET: Find_All_User_Failed', async () => {
-    const result = await request(app.getHttpServer()).get('/users');
-    // console.log(result);
+  it('GET: Find_All_User_Unauthorized_Failed', async () => {
+    const result = await request(app.getHttpServer()).get(
+      '/users?page=1&pageSize=5',
+    );
     expect(result.statusCode).toBe(401);
     expect(result.body.message).toStrictEqual('Unauthorized');
   });
 
   // Update_User
+  it('PATCH: Update_User_Success', async () => {
+    const result = await request(app.getHttpServer())
+      .patch('/users/1')
+      .send({
+        username: 'testingUser12',
+        email: 'testingEmail12@google.com',
+        password: 'testingPassword123',
+        role: 'admin',
+      })
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(result.statusCode).toBe(200);
+    expect(result.body.success).toBe(true);
+  });
+  it('PATCH: Update_User_Unauthorized_Failed', async () => {
+    const result = await request(app.getHttpServer()).patch('/users/1').send({
+      username: 'testingUser12',
+      email: 'testingEmail12@google.com',
+      password: 'testingPassword123',
+      role: 'admin',
+    });
+    expect(result.statusCode).toBe(401);
+    expect(result.body.message).toStrictEqual('Unauthorized');
+  });
+  it('PATCH: Update_User_Not_Found_Failed', async () => {
+    const result = await request(app.getHttpServer())
+      .patch('/users/2')
+      .send({
+        username: 'testingUser12',
+        email: 'testingEmail12@google.com',
+        password: 'testingPassword123',
+        role: 'admin',
+      })
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(result.statusCode).toBe(404);
+    expect(result.body.message).toStrictEqual(
+      'There is no user record corresponding to this userIndex - 2',
+    );
+  });
 
   // Remove_User
-
-  // Test 수행 후, DB synchronize로 DB 초기화
-  // afterAll(async () => {
-  //   await app.get(DataSource).synchronize(true);
-  // });
+  it('DELETE: Remove_User_Success', async () => {
+    const result = await request(app.getHttpServer())
+      .delete('/users/1')
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(result.statusCode).toBe(200);
+    expect(result.body.success).toBe(true);
+  });
+  it('DELETE: Remove_User_Unauthorized_Failed', async () => {
+    const result = await request(app.getHttpServer()).delete('/users/1');
+    expect(result.statusCode).toBe(401);
+    expect(result.body.message).toStrictEqual('Unauthorized');
+  });
+  it('DELETE: Remove_User_Not_Found_Failed', async () => {
+    const result = await request(app.getHttpServer())
+      .delete('/users/1')
+      .set('Authorization', `Bearer ${accessToken}`);
+    expect(result.statusCode).toBe(404);
+    expect(result.body.message).toStrictEqual(
+      'There is no user record corresponding to this userIndex- 1',
+    );
+  });
 });
